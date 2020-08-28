c81261d44637ea74a03d59b80463819ad66542be
Tietohallinto <tietohallinto@BC-minimac051.local>
2020-08-28 12:30:51 +0300

    update customScript.js

--- js/customScript.js
+++ js/customScript.js
@@ -1,1 +1,7 @@
 console.log("Hello from customScript.js!");
+
+document.getElementById('slogan').innerText = 'We hate our customers';
+
+document.getElementById('all').style.backgroundColor = '#c7c3c3';
+
+document.body.style.fontFamily = 'impact';
